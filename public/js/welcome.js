5351959388dd42f8bcd32a0502691e99e0eea6c1
flatironschool <t.benson@live.co.uk>
2020-02-12 09:24:43 +0000

    Created landing page with login and create user functionality

--- public/js/welcome.js
+++ public/js/welcome.js
@@ -1,7 +1,7 @@
 // helper functions
 const getElement = (element) => document.querySelector(element)
 // urls
-const usersURL = "localhost:3000/users/"
+const usersURL = "http://localhost:3000/users/"
 
 // elements
 const gameHeader = getElement("#gameHeader")
@@ -12,18 +12,24 @@ const loginDiv = getElement("#loginDiv")
 const gameContainer = getElement("#gameContainer")
 const signUpButton = getElement("#signUpButton")
 const loginButton = getElement("#loginButton")
+const loginForm = getElement("#loginForm")
+const signupForm = getElement("#signupForm")
+const loggedInPage = getElement("#loggedInPage")
 
+
+const userDetails = {}
 
 
 // make item hidden
-const hideItem = (item) => item.style.display = "none"
-const unHideItem = (item) => item.style.display = "flex"
+const hideItem = (item) => item.classList.add("hidden")
+const unHideItem = (item) => item.classList.remove("hidden")
 
 // first screen hide everything but buttons for login or signup
 const firstScreen = () => {
     hideItem(signUpDiv)
     hideItem(loginDiv)
     hideItem(gameContainer)
+    hideItem(loggedInPage)
 
     alien = document.createElement("img")
     alien.src = "/assets/alien.png"
@@ -32,39 +38,114 @@ const firstScreen = () => {
     welcomeDiv.append(alien)
 
 }
-
 firstScreen()
 
+
+// show sign up form
 signUpButton.addEventListener('click', (e) => {
     hideItem(welcomeDiv)
     unHideItem(signUpDiv)
-
-
 })
 
+// show log in form
 loginButton.addEventListener('click', (e) => {
     hideItem(welcomeDiv)
     unHideItem(loginDiv)
 })
 
-const loginForm = getElement("#loginForm")
 
+// find user when logging in
 loginForm.addEventListener("submit", (e) => {
-    newUsername = loginForm.querySelector(".username")
+    e.preventDefault()
+    userForm = loginForm.querySelector(".username").value.toUpperCase()
     
+    e.target.reset()
 
-    const configObj = {
-        method: "GET",
-        headers: { "content-type": "application/json"},
-        body: {
-            
-        }
-    }
     fetch(usersURL)
         .then( resp => resp.json() )
-        .then()
+        .then( users => { findUser(users, userForm)} )
+})
+// find a user
+const findUser = (users, userForm) => {
+    for(const user of users){
+        userName = user.username.toUpperCase()
+        if (userName == userForm) {
+            userDetails.username = userName
+            userDetails.id = user.id
+            return loggedIn()
+        }
+    }
+    userNotFound()
+}
+// not found
+const userNotFound = () => {
+    if (!getElement("#notFound")) {
+        notFound = document.createElement("div")
+        notFound.innerText = "Sorry, we couldn't find that user, try again!"
+        notFound.style.color = "#00ff00"
+        notFound.id = "notFound"
+        notFound.classList = "center-item"
+        welcomeDiv.append(notFound)
+    }
+    hideItem(loginDiv)
+    unHideItem(welcomeDiv)
+}
 
-    e.target.reset
+// create user
+signupForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    userForm = signupForm.querySelector(".username").value.toUpperCase()
+    
+    e.target.reset()
+
+    fetch(usersURL)
+        .then( resp => resp.json() )
+        .then( users => { createUser(users, userForm)} )
 })
 
+const createUser = (users, userForm) => {
+    for(const user of users){
+        userName = user.username.toUpperCase()
+        if (userName == userForm) {
+            return userExists()
+        }
+    }
+    
+    const configObj = {
+        method: "POST",
+        headers: {"content-type": "application/json"},
+        body: JSON.stringify({
+            username: userForm
+        })
+    }
 
+    fetch(usersURL, configObj)
+        .then( resp => resp.json())
+        .then( user => {
+            userDetails.username = user.username
+            userDetails.id = user.id
+            return loggedIn()
+        } ) 
+}
+
+// user already exists
+const userExists = () => {
+    if (!getElement("#userTaken")) {
+        userTaken = document.createElement("div")
+        userTaken.innerText = "Sorry, that username is already taken! Please choose a new one or log in!"
+        userTaken.id = "userTaken"
+        userTaken.style.color = "#00ff00"
+        userTaken.classList = "center-item"
+        welcomeDiv.append(userTaken)
+    }
+    hideItem(signUpDiv)
+    unHideItem(welcomeDiv)
+}
+
+// take them to the logged in page
+const loggedIn = () => {
+    hideItem(signUpDiv)
+    hideItem(loginDiv)
+    unHideItem(loggedInPage)
+    getElement("#name").innerText = userDetails.username
+}
